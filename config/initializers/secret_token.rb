367755462613a7b22325267bc884c3134c696d20
bartender75 <bartender75@gmail.com>
2012-05-25 19:39:43 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idrink::Application.config.secret_token = '9a998d3441d81aa5cfeb6f783e7fc2054d8c0cc740d637e4690eb63e8d39dcc731b5272f017e0b9f884ae44a3c738bfc9129eb8d2db0db85a068c98a013ee23d'
---
+++ db/migrate/20120525105914_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :sex
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
